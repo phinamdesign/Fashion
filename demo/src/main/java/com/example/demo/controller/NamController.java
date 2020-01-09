eb4642ee6aa159421bdfb0e35f7fdb9f742a4549
nghia-code1805 <you@example.com>
2020-01-09 09:57:00 +0700

    nghaqqwe

--- demo/src/main/java/com/example/demo/controller/NamController.java
+++ demo/src/main/java/com/example/demo/controller/NamController.java
@@ -3,5 +3,6 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public class NamController {
+    private String nghiaxnun;
     private String name;
 }
